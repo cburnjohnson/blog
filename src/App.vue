b7f6802c08a9fab6bb4aa25fc1d6454687aa15cb
Cole Johnson <colejohnson@Coles-MBP.hsd1.or.comcast.net>
2021-04-15 10:29:23 -0700

    add blog

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,28 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <add-blog @addBlog="addBlog"></add-blog>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AddBlog from "./components/AddBlog";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    AddBlog
+  },
+  data: function() {
+    return {
+      blogs: []
+    };
+  },
+  methods: {
+    addBlog(blog) {
+      this.blogs = [...this.blogs, blog];
+    }
   }
-}
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
---
+++ src/components/AddBlog.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <form>
+      <label>Title</label>
+      <input type="text" id="blog-title" v-model="blog.title" />
+      <label>Blog Content</label>
+      <textarea id="blog-content" v-model="blog.content"></textarea>
+      <label>Author</label>
+      <select v-model="blog.author">
+        <option value="Cole">Cole</option>
+        <option value="Kam">Kam</option>
+      </select>
+      <input type="submit" value="Post Blog" @click.prevent="addBlog" />
+    </form>
+
+    <div id="preview">
+      <h2>Blog Title: {{ blog.title }}</h2>
+      <p>Blog Content: {{ blog.content }}</p>
+      <p>Blog Author: {{ blog.author }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      blog: {
+        title: "",
+        content: "",
+        author: ""
+      }
+    };
+  },
+  methods: {
+    addBlog() {
+      this.$emit("addBlog", this.blog);
+    }
+  }
+};
+</script>
